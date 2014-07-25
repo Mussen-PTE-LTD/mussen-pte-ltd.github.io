f593d1ccec84c37660ccd6c1c356d87ac9d6e334
Yao Wei Tjong 姚伟忠 <weitjong@gmail.com>
2014-07-25 14:07:16 +0800

    Products list element is affixed on larger viewport.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-
+function affixWidth() {
+    $('#products-list').width($('#products-list-parent').width());
+}
+$(document).ready(function () {
+    affixWidth();
+    $(window).resize(affixWidth);
+});
